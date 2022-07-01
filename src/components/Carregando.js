decdc53793280d04392dc05a7a527b07a7d3fa84
Patricia Correia <pncorreia25@gmail.com>
2022-07-01 18:58:09 -0300

    requisito 2

---
+++ src/components/Carregando.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Carregando extends React.Component {
+  render() {
+    return (<h3>Carregando...</h3>);
+  }
+}
+export default Carregando;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Carregando from '../components/Carregando';
+
+const minimoCharacter = 3;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      carregando: false,
+    };
+  }
+
+  loading = () => {
+    this.setState((prevState) => ({
+      carregando: !prevState.carregando,
+    }));
+  }
+
+  handelInput = ({ target }) => this.setState({ name: target.value });
+
+  userValidation = async ({ name }) => {
+    const { history } = this.props;
+    this.loading();
+    await createUser({ name });
+    history.push('/search');
+  }
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    const { name, carregando } = this.state;
+    const validateLogin = name.length >= minimoCharacter;
+    if (carregando) return <Carregando />;
+
+    return (
+      <div data-testid="page-login">
+        <fildset>
+          Login
+          <label htmlFor="login">
+            Name:
+            <input
+              data-testid="login-name-input"
+              type="text"
+              name="login"
+              onChange={ this.handelInput }
+            />
+          </label>
+          <button
+            type="submit"
+            onClick={ () => this.userValidation({ name }) }
+            disabled={ !validateLogin }
+            data-testid="login-submit-button"
+          >
+            Entrar
+          </button>
+        </fildset>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
