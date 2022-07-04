1908a9f931efdffca605a67fe0c6821af3714fed
Patricia Correia <pncorreia25@gmail.com>
2022-07-04 16:07:35 -0300

    requisito 5 test

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -31,7 +31,6 @@ class Search extends React.Component {
         <button
           type="submit"
           data-testid="search-artist-button"
-          onClick={ () => this.userValidation({ name }) }
           disabled={ !validateLogin }
         >
           Pesquisar
